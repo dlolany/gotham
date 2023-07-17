64c6633b737d712b0b761b9d3b0f17458cea2b8b
danyklein7554 <62357139+danyklein7554@users.noreply.github.com>
2023-07-17 12:04:53 +0200

    push

---
+++ disc/router.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from 'ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _ae73d79c = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _7a23e8e0 = () => interopDefault(import('../pages/tables/_id.vue' /* webpackChunkName: "pages/tables/_id" */))
+
+const emptyFn = () => {}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/",
+    component: _ae73d79c,
+    name: "index"
+  }, {
+    path: "/tables/:id?",
+    component: _7a23e8e0,
+    name: "tables-id"
+  }],
+
+  fallback: false
+}
+
+export function createRouter (ssrContext, config) {
+  const base = (config._app && config._app.basePath) || routerOptions.base
+  const router = new Router({ ...routerOptions, base  })
+
+  // TODO: remove in Nuxt 3
+  const originalPush = router.push
+  router.push = function push (location, onComplete = emptyFn, onAbort) {
+    return originalPush.call(this, location, onComplete, onAbort)
+  }
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    return resolve(to, current, append)
+  }
+
+  return router
+}
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+
+  buildDir: 'disc',
   head: {
     title: 'dany',
     htmlAttrs: {
